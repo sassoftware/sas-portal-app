c9c630878069f867972a881ba8a2e8f7846703e8
Craig Rubendall <Craig.Rubendall@sas.com>
2023-07-24 10:40:59 -0400

    fix issue with default tab not showing up on first load of page

--- web/custom-sasjs-scripts.js
+++ web/custom-sasjs-scripts.js
@@ -29,9 +29,13 @@ function reject() {
   console.log('in reject');
 }
 
+/*
+ *  Now that we know we have a sessions, fully generate the html content
+ */
+
 function afterLogin() {
 
-//console.log('in afterLogin');
+  // console.log('in afterLogin');
 
   /* since we are only support logging in via redirection, no need for login form
   const loginForm = document.getElementById('login-form')
@@ -45,17 +49,28 @@ function afterLogin() {
    */
 
   //console.log('generating html');
-  includeHTML();
+  includeHTML(afterGeneration);
+
+  }
+
+/*
+ *  This function runs as a callback after the
+ *  html inclusion/generation has been executed.
+ */
+
+function afterGeneration() {
+
+// console.log('afterGeneration:');
 
   //console.log('selecting default tab');
   defaultTab=document.getElementById("default-tab");
-  //console.log('init: found default-tab:'+defaultTab);
+  // console.log('init: found default-tab:'+defaultTab);
 
   if (defaultTab) {
       defaultTab.click();
       }
   else {
-     //console.log('No default tab found?');
+     console.log('No default tab found?');
      }
 
   /*
@@ -149,3 +164,79 @@ window.onload = function () {
       console.error(err)
     })
 }
+
+function showTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+	tabcontent[i].className = tabcontent[i].className.replace(" active", "");
+    }
+    // Get all elements representing portal tabs and unselect them
+    // NOTE: I tried to optimize this to only change info on the one that had the selected class
+    //       but because the collection is dynamic, as soon as I modified the class value that
+    //       was used for searching, the length of the collection changed.
+
+    tablinks = document.getElementsByClassName("tab-button");
+
+    numActiveTabs=tablinks.length;
+
+    for (i = 0; i < tablinks.length; i++) {
+	tablinks[i].className = tablinks[i].className.replace("BannerTabButtonActive", "BannerTabButtonCenter");
+
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+
+    document.getElementById(tabName).className += " active";
+
+    // Change the class settings on the selected object to change the styles applied
+    tabButton=evt.currentTarget;
+
+    tabButton.className = tabButton.className.replace("BannerTabButtonCenter", "BannerTabButtonActive");
+
+}
+
+function includeHTML(cb) {
+
+// console.log("includeHTML: start");
+
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+	if (this.readyState == 4) {
+	  if (this.status == 200) {
+	     elmnt.innerHTML = this.responseText;
+	     }
+	  if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+	  /* Remove the attribute, and call this function once more: */
+	  elmnt.removeAttribute("w3-include-html");
+	  includeHTML(cb);
+	}
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+
+/*
+ *  Now call the callback if one was passed 
+ */
+
+if (cb) cb();
+
+}
+
